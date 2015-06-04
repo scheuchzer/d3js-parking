882628fd82336d088a3c52e22ca0ccce835060eb
Thomas Scheuchzer <thomas.scheuchzer@gmx.net>
2015-06-04 14:05:04 +0200

    update

--- app/parkingController.js
+++ app/parkingController.js
@@ -1,27 +1,29 @@
 'use strict';
 
 angular.module('parkingApp')
-    .controller('ParkingController', function ($scope, $http, $log, $filter) {
+    .controller('ParkingController', function ($scope, $http, $log, $filter, $interval) {
         var centers = [];
+        $scope.parkings = {};
 
-        var width = 1960,
-            height = 1500;
+        var width = 1000,
+            height = 500;
 
+        var color = d3.scale.linear().domain([0, 100]).range(['red', 'green']);
         var fill = d3.scale.log()
-            .domain([10, 500])
+            .domain([10, 400])
             .range(["brown", "steelblue"]);
 
         var path = d3.geo.path();
 
-        var svg = d3.select("body").append("svg")
+        var svg = d3.select("#map").append("svg")
             .attr("width", width)
             .attr("height", height);
 
-        var div = d3.select('body').append("div")
+        var div = d3.select('#map').append("div")
             .attr("class", "tooltip")
             .style("opacity", 0);
 
-        var stadtkreisText = d3.select('body').append("div")
+        var stadtkreisText = d3.select('#map').append("div")
             .attr("class", "stadtkreisText")
             .style("opacity", 1);
 
@@ -96,17 +98,25 @@ angular.module('parkingApp')
                         return projection([coords[0], coords[1]])[1];
                     })
                     .attr("r", 5)
-                    .style("fill", "red")
                     .on('mouseover', function(d,i){
+
+                        var free = 'unbekannt';
+                        var parking = $scope.parkings[d.properties.Name];
+                        if (parking) {
+                            free = parking.free;
+                        }
                         d3.select(this)
                             .transition()
                             .duration(200)
-                            .style('fill', 'green')
-                            .attr('r', '10');
+                            .attr('r', '15');
                         div.transition()
                             .duration(200)
                             .style("opacity", .9);
-                        div.html(d.properties.Name + '<br/>'+ d.properties.Adresse)
+                        div.html(d.properties.Name
+                            + '<br/>'
+                            + d.properties.Adresse
+                            + '<br/>'
+                            + 'Frei: ' + free)
                             .style("left", (d3.event.pageX + 10 ) + "px")
                             .style("top", (d3.event.pageY ) + "px");
                     })
@@ -115,12 +125,28 @@ angular.module('parkingApp')
                         d3.select(this)
                             .transition()
                             .duration(500)
-                            .style('fill', 'red').attr('r', '5');
+                            .attr('r', '5');
                         div.transition()
                             .duration(500)
                             .style("opacity", 0);
                     });
+
+                $scope.$watch('parkings', function(){
+                    pg.selectAll("circle")
+                        .data(data.features)
+                        .transition()
+                        .duration(200)
+                        .style("fill", function(d) {
+                            var parking = $scope.parkings[d.properties.Name];
+                            if (parking) {
+                                return color(parking.free);
+                            }
+                            return 'gray';
+                        });
+
+                }, true);
             });
+
 
             function zoomed() {
                 projection.translate(d3.event.translate).scale(d3.event.scale);
@@ -135,9 +161,72 @@ angular.module('parkingApp')
                         return projection([coords[0], coords[1]])[1];
                     })
                     .attr("r", 5);
+            };
+
+            d3.xml("rss.xml", "application/xml", initializeParkings);
+            function initializeParkings(error, data) {
+                var elements = cleanFeedForPlot(data);
+                elements.forEach(function(d) {
+                    var name = d.name;
+                    name = name.replace('Parkhaus ','');
+                    name = name.replace('Parkplatz ','');
+                    name = name.replace('Parkgarage am ','');
+                    $scope.parkings[name] = { name: name, free: d.freeSpaces};
+                });
+                $scope.$apply();
+
             }
+
+            function cleanFeedForPlot(data) {
+                var elementsPlusOtherStuff = data.children[0].children[0].children;
+                var elementsWithoutStuff = cleanFeed(elementsPlusOtherStuff);
+                var elements = extractInterestingInfo(elementsWithoutStuff);
+
+                return elements;
+            }
+
+            function cleanFeed(elementsWithStuff) {
+                var elementsWithoutStuff = [];
+                for (var i = 0; i < elementsWithStuff.length; i++) {
+                    var element = elementsWithStuff[i];
+                    if (element.tagName === "item") {
+                        elementsWithoutStuff.push(element);
+                    }
+                }
+
+                return elementsWithoutStuff;
+            }
+
+            function extractInterestingInfo(dirtyElements) {
+                return dirtyElements.map(function(e) {
+                    var nameAdress = e.getElementsByTagName("title")[0].innerHTML;
+                    var status = e.getElementsByTagName("description")[0].innerHTML;
+
+                    return {
+                        name: extractName(nameAdress),
+                        freeSpaces: extractFreeSpaces(status)
+                    }
+                });
+
+                function extractName(nameAdress) {
+                    return nameAdress.split("/")[0].trim();
+                }
+
+                function extractFreeSpaces(status) {
+                    return Number.parseInt(status.split("/")[1].trim());
+                }
+            }
+
+
 
         });
 
+        $interval(function(){
+            angular.forEach($scope.parkings, function(d){
+                d.free = Math.max(0, Math.round(d.free +Math.random()*40-20));
+                $scope.parkings[d.name] = d;
+            });
+
+        }, 1000);
 
     });
